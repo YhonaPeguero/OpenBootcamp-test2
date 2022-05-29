27f3bbb1f177178d275362b8b0e2e030c325bf15
Yhonatan Peguero <2017-0516@unad.edu.do>
2022-05-29 15:13:35 -0400

    first commit

---
+++ test2.js
@@ -0,0 +1,17 @@
+const dates = [
+  "Yhonatan",
+
+  24,
+
+  true,
+
+  (birthDay = new Date("July 20 1998")),
+  (bookFavorite = {
+    titulo: "Harry Potter",
+    autor: "J.K. Rowling",
+    ano: 1997,
+    url: "https://www.amazon.com.br/Harry-Potter-J-K-Rowling/dp/059035342X/ref=sr_1_1?__mk_pt_BR=%C3%85M%C3%85%C5%BD%C3%95%C3%91&keywords=harry+potter&qid=1599079078&s=books&sr=1-1",
+  }),
+];
+
+console.log(dates);
